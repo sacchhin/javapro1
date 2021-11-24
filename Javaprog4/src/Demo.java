dc9c22bded9af5860b08ddf5b06f0254296168c9
sachinsoppin <sachinsoppin@LAPTOP-UPG3F4KF>
2021-11-24 10:05:06 +0530

    first commit

---
+++ Javaprog4/src/Demo.java
@@ -0,0 +1,16 @@
+
+public class Demo 
+{
+	public void value(int x)
+	{
+		System.out.println("interger value: "+x);
+	}
+    public void value(String x)
+    {
+    	System.out.println("string value: "+x);
+    }
+    public void value(double x)
+    {
+    	System.out.println("double value: "+x);
+    }
+}
---
+++ Javaprog4/src/Ex1.java
@@ -0,0 +1,12 @@
+
+public class Ex1 {
+
+	public static void main(String[] args) 
+	{
+		Demo Obj=new Demo();
+		Obj.value(10);
+		Obj.value("sachin");
+		Obj.value(3.56);
+	}
+
+}
